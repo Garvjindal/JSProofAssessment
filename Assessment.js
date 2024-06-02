0d79c096c1657f5f13489a8d8e07800375fa32da
Garvjindal <146101683+Garvjindal@users.noreply.github.com>
2024-06-03 00:10:56 +0530

    Update Assessment.js

--- Assessment.js
+++ Assessment.js
@@ -1,17 +1,18 @@
 // Create a variable to hold NFTs
 let nftCollection = [];
 
-// Function to mint NFT
-function mintNFT(id, name, brand, price, quantity) {
-    // Create item object with metadata
+// Function to mint NFT (now representing stationary items)
+function mintNFT(id, name, brand, category, price, quantity) {
+    // Create stationary item object with metadata
     let newNFT = {
         id: id,
         name: name,
         brand: brand,
+        category: category,
         price: price,
         quantity: quantity,
     };
-    // Store item in collection
+    // Store stationary item in collection
     nftCollection.push(newNFT);
 }
 
@@ -23,6 +24,7 @@ function listNFTs() {
         console.log("ID: " + nft.id);
         console.log("Name: " + nft.name);
         console.log("Brand: " + nft.brand);
+        console.log("Category: " + nft.category);
         console.log("Price: " + nft.price);
         console.log("Quantity: " + nft.quantity);
         console.log("-----------------------");
@@ -35,16 +37,16 @@ function getTotalSupply() {
     return nftCollection.length;
 }
 
-// Mint some  items
+// Mint some stationary items
 mintNFT(1, "Gel Pen", "Pilot", "Pen", "$2", 100);
 mintNFT(2, "A4 Notebook", "Moleskine", "Notebook", "$15", 50);
 mintNFT(3, "Eraser", "Faber-Castell", "Eraser", "$1", 200);
 mintNFT(4, "Highlighter", "Sharpie", "Marker", "$3", 75);
 
 // List stationary items
-console.log("Listing NFT Items:");
+console.log("Listing Stationary Items:");
 console.log("-----------------------");
 listNFTs();
 
 // Print total supply
-console.log("Total NFT items: " + getTotalSupply());
+console.log("Total stationary items: " + getTotalSupply());
